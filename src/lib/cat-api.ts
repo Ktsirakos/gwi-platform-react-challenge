038d1d08f1c5bf73a1d48f34b867fe36c9384406
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-12 15:55:49 +0200

    feat: enhancing api calls from cat api - adding filter by breed functionality

--- src/lib/cat-api.ts
+++ src/lib/cat-api.ts
@@ -1,13 +1,20 @@
 import { fetchClient } from "./api-client"
+import { Breed, Cat } from "@/types/cat-api-types"
 
 export const fetchRandomCats = async ({
-    limit = 10
-}: { limit?: number }) => {
-    const response = await fetchClient(`/images/search/?limit=${limit}`)
+    limit = 10,
+    filteredBreeds = []
+}: { limit?: number, filteredBreeds?: string[] }) => {
+    const filterByBreedQueryParams = filteredBreeds.length ? `?breed_ids=${filteredBreeds.join(",")}` : ''
+
+    const response = await fetchClient(
+        `/images/search/?limit=${limit}${filterByBreedQueryParams}`
+    ) as Cat[]
+
     return response
 }
 
 export const fetchBreeds = async () => {
-    const response = await fetchClient("/breeds")
+    const response = await fetchClient("/breeds") as Breed[]
     return response
 }
