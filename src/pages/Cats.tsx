ab2a4e4e07c36094e914890913c6344540022e11
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-12 15:56:19 +0200

    feat: creating cat card - shows loader while image is loading - adding cat card on cats page

--- src/pages/Cats.tsx
+++ src/pages/Cats.tsx
@@ -1,5 +1,21 @@
+import CatCard from "@/components/CatCard"
+import { fetchRandomCats } from "@/lib/cat-api"
+import { Cat } from "@/types/cat-api-types"
+import { useEffect, useState } from "react"
+
 export default function Cats() {
+    const [cats, setCats] = useState<Cat[]>([])
+    useEffect(() => {
+        fetchRandomCats({}).then((value) => setCats(value))
+    }, [])
+
     return (
-        <p className="text-5xl text-black">Cats</p>
+        <div className="flex-1">
+            <div className="grid grid-cols-5 gap-2 ">
+                {
+                    cats.map((cat) => <CatCard key={cat.id} cat={cat} />)
+                }
+            </div>
+        </div>
     )
 }
