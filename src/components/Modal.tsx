d90d3a9128083e40ba28e44e06d83a65b9d2d9e0
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-12 23:47:08 +0200

    feat: adding reusable modal component - adding modal to Cats page - created correct flow of pressing CatCards

---
+++ src/components/Modal.tsx
@@ -0,0 +1,34 @@
+import { X } from "lucide-react";
+import { ReactNode } from "react";
+
+export default function Modal({
+    children,
+    title,
+    onClose,
+    isOpen
+}: {
+    children: ReactNode,
+    title: string,
+    onClose: () => void,
+    isOpen: boolean
+}
+) {
+
+    if (!isOpen) return null;
+
+    return (
+        <div className="fixed z-10 inset-0 w-full h-full">
+            <div className="absolute p-5 border-1 w-full h-full bg-gray-700 opacity-50">
+            </div>
+            <div className="absolute inset-0 z-12 flex flex-col justify-self-center self-center w-2/4 h-9/10 bg-white rounded-2xl overflow-hidden">
+                <div className="flex-1 flex flex-row items-center justify-between p-5">
+                    <p className="text-black text-3xl">{title}</p>
+                    <button className="text-gray-500 text-5xl hover:text-primary" onClick={onClose}><X /></button>
+                </div>
+                <div className="flex-11">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Cats.tsx
+++ src/pages/Cats.tsx
@@ -1,17 +1,28 @@
 import AsyncButton from "@/components/AsyncButton"
 import CatCard from "@/components/CatCard"
+import Modal from "@/components/Modal"
+import { ROUTES } from "@/config/routes"
 import { fetchRandomCats } from "@/lib/cat-api"
 import { Cat } from "@/types/cat-api-types"
 import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router"
 
 export default function Cats() {
     const [cats, setCats] = useState<Cat[]>([])
     const [fetchingMoreCats, setFetchingMoreCats] = useState(false)
+    const [showCatDetails, setShowCatDetails] = useState(false)
+    const navigate = useNavigate()
+    const { id: catId } = useParams()
 
     useEffect(() => {
         fetchRandomCats({}).then((value) => setCats(value))
     }, [])
 
+
+    useEffect(() => {
+        if (catId) setShowCatDetails(true)
+        else setShowCatDetails(false)
+    }, [catId])
 
     const fetchMoreCats = () => {
         setFetchingMoreCats(true)
@@ -22,15 +33,25 @@ export default function Cats() {
     }
 
     return (
-        <div className="flex-1">
-            <div className="grid grid-cols-5 gap-2 mb-5">
-                {
-                    cats.map((cat) => <CatCard key={cat.id} cat={cat} />)
-                }
+        <>
+            <div className="flex-1">
+                <div className="grid grid-cols-5 gap-2 mb-5">
+                    {
+                        cats.map((cat) => <CatCard key={cat.id} cat={cat} onClick={() => {
+                            navigate(ROUTES.CAT_DETAILS(cat.id))
+                        }} />)
+                    }
+                </div>
+                <div className="flex mb-10 w-full justify-center">
+                    <AsyncButton onClick={fetchMoreCats} loading={fetchingMoreCats} />
+                </div>
+
             </div>
-            <div className="flex mb-10 w-full justify-center">
-                <AsyncButton onClick={fetchMoreCats} loading={fetchingMoreCats} />
-            </div>
-        </div>
+            <Modal isOpen={showCatDetails} title="Cat Details" onClose={() => {
+                navigate(ROUTES.CATS)
+            }}>
+                <p className="text-black">Cat Details content</p>
+            </Modal>
+        </>
     )
 }
