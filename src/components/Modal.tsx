f271aee078f2c1da9fdd9fea7ae6eca0e60bdf79
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-15 22:04:05 +0200

    chore: minor changes - website title - making modal title black

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -22,7 +22,7 @@ export default function Modal({
             </div>
             <div className="absolute inset-0 z-12 flex flex-col justify-self-center self-center w-2/4 h-9/10 bg-white rounded-2xl overflow-scroll">
                 <div className="flex-1 flex flex-row items-center justify-between p-5">
-                    <p className="text-3xl">{title}</p>
+                    <p className="text-3xl text-black">{title}</p>
                     <button className="text-gray-500 text-5xl hover:text-primary" onClick={onClose}><X /></button>
                 </div>
                 <div className="flex-11 mb-10">
